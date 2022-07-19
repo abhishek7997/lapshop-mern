a347588dfd565459c0604d0a8ab837a0f3ffa213
abhishek7997 <68701271+abhishek7997@users.noreply.github.com>
2022-07-19 17:19:00 +0530

    Deploy settings changed

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:4000",
-      changeOrigin: true,
-    })
-  )
+  if (process.env.NODE_ENV !== "PRODUCTION") {
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      })
+    )
+  }
 }
